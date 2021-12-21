0146847292951dbcfe70b641ea096116d7c5d91b
jhcanti <jhcanti@gmail.com>
2021-12-21 22:28:25 +0100

    Update EnemyDestroyedEvent.cs

--- Assets/Code/Common/Events/EnemyDestroyedEvent.cs
+++ Assets/Code/Common/Events/EnemyDestroyedEvent.cs
@@ -1,11 +1,9 @@
 public class EnemyDestroyedEvent : EventData
 {
     public readonly int PointsToAdd;
-    public readonly int InstanceId;
 
-    public EnemyDestroyedEvent(int points, int instanceId) : base(EventIds.EnemyDestroyed)
+    public EnemyDestroyedEvent(int points) : base(EventIds.EnemyDestroyed)
     {
         PointsToAdd = points;
-        InstanceId = instanceId;
     }
 }
