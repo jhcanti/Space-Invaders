f5cefb4b7aacd0713dd27e1be3fa0f7f07958699
jhcanti <jhcanti@gmail.com>
2021-12-21 23:02:18 +0100

    Update PlayingState.cs

--- Assets/Code/FSM/GameStates/PlayingState.cs
+++ Assets/Code/FSM/GameStates/PlayingState.cs
@@ -1,6 +1,9 @@
 ﻿public class PlayingState : IState, IEventObserver
 {
     private readonly GameManager _gameManager;
+    private EventQueue _eventQueue;
+    private int _aliveEnemies;
+    private bool _allEnemiesSpawned;
 
     public PlayingState(GameManager gameManager)
     {
@@ -9,19 +12,46 @@
 
     public void Tick()
     {
+        if (_aliveEnemies == 0 && _allEnemiesSpawned)
+        {
+            _gameManager.CurrentGameState = GameStates.Victory;
+        }
     }
 
     public void OnEnter()
     {
+        _eventQueue = ServiceLocator.Instance.GetService<EventQueue>();
+        _eventQueue.Subscribe(EventIds.EnemyDestroyed, this);
+        _aliveEnemies = 0;
+        _allEnemiesSpawned = false;
         new LoadSceneCommand("Game").Execute();
     }
 
     public void OnExit()
     {
+        _eventQueue.Unsubscribe(EventIds.EnemyDestroyed, this);
     }
 
     public void Process(EventData eventData)
     {
-            
+        if (eventData.EventId == EventIds.EnemySpawned)
+        {
+            _aliveEnemies++;
+        }
+
+        if (eventData.EventId == EventIds.EnemyDestroyed)
+        {
+            _aliveEnemies--;
+        }
+
+        if (eventData.EventId == EventIds.AllEnemiesSpawned)
+        {
+            _allEnemiesSpawned = true;
+        }
+
+        if (eventData.EventId == EventIds.PlayerDestroyed)
+        {
+            _gameManager.CurrentGameState = GameStates.GameOver;
+        }
     }
 }
