7dd88cc5b0ef60e933c517584ee0b02b7364c2c6
jhcanti <jhcanti@gmail.com>
2021-12-21 23:06:38 +0100

    Update PlayingState.cs

--- Assets/Code/FSM/GameStates/PlayingState.cs
+++ Assets/Code/FSM/GameStates/PlayingState.cs
@@ -21,7 +21,10 @@
     public void OnEnter()
     {
         _eventQueue = ServiceLocator.Instance.GetService<EventQueue>();
+        _eventQueue.Subscribe(EventIds.EnemySpawned, this);
         _eventQueue.Subscribe(EventIds.EnemyDestroyed, this);
+        _eventQueue.Subscribe(EventIds.AllEnemiesSpawned, this);
+        _eventQueue.Subscribe(EventIds.PlayerDestroyed, this);
         _aliveEnemies = 0;
         _allEnemiesSpawned = false;
         new LoadSceneCommand("Game").Execute();
@@ -29,7 +32,10 @@
 
     public void OnExit()
     {
+        _eventQueue.Unsubscribe(EventIds.EnemySpawned, this);
         _eventQueue.Unsubscribe(EventIds.EnemyDestroyed, this);
+        _eventQueue.Unsubscribe(EventIds.AllEnemiesSpawned, this);
+        _eventQueue.Unsubscribe(EventIds.PlayerDestroyed, this);
     }
 
     public void Process(EventData eventData)
