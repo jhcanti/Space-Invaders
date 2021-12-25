73830ce63971ba9435b7f65ef484097ddd06bd51
jhcanti <jhcanti@gmail.com>
2021-12-25 16:22:31 +0100

    Elimino boton Menu en Game Over

--- Assets/Code/FSM/GameStates/GameOverState.cs
+++ Assets/Code/FSM/GameStates/GameOverState.cs
@@ -2,7 +2,6 @@
 {
     private readonly GameManager _gameManager;
     private EventQueue _eventQueue;
-    private bool _menuButtonPressed;
     private bool _restartButtonPressed;
 
     public GameOverState(GameManager gameManager)
@@ -12,19 +11,14 @@
     
     public void Tick()
     {
-        if (_menuButtonPressed)
-            _gameManager.CurrentGameState = GameStates.InMenu;
-
         if (_restartButtonPressed)
             _gameManager.CurrentGameState = GameStates.RestartLevel;
     }
 
     public void OnEnter()
     {
-        _menuButtonPressed = false;
         _restartButtonPressed = false;
         _eventQueue = ServiceLocator.Instance.GetService<EventQueue>();
-        _eventQueue.Subscribe(EventIds.BackToMenu, this);
         _eventQueue.Subscribe(EventIds.RestartPressed, this);
         _eventQueue.EnqueueEvent(new GameOverEvent());
         new StopAndResetCommand().Execute();
@@ -32,17 +26,11 @@
 
     public void OnExit()
     {
-        _eventQueue.Unsubscribe(EventIds.BackToMenu, this);
         _eventQueue.Unsubscribe(EventIds.RestartPressed, this);
     }
 
     public void Process(EventData eventData)
     {
-        if (eventData.EventId == EventIds.BackToMenu)
-        {
-            _menuButtonPressed = true;
-        }
-
         if (eventData.EventId == EventIds.RestartPressed)
         {
             _restartButtonPressed = true;
