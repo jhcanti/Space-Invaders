6afc8befa63346bdfca7f39287d2706fb9a10707
jhcanti <jhcanti@gmail.com>
2021-12-31 12:31:30 +0100

    Ahora hay proyectiles que tienen un coste por segundo

--- Assets/Code/Proyectiles/ProjectileId.cs
+++ Assets/Code/Proyectiles/ProjectileId.cs
@@ -5,7 +5,9 @@ public class ProjectileId : ScriptableObject
 {
     [SerializeField] private string projectileId;
     [SerializeField] private float fireRate;
+    [SerializeField] private float costPerSecond;
 
     public string Value => projectileId;
     public float FireRate => fireRate;
+    public float CostPerSecond => costPerSecond;
 }
