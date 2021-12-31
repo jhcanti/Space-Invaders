1b188f953d39dc4ec11a7a345336e4504c97ea59
jhcanti <jhcanti@gmail.com>
2021-12-31 12:24:54 +0100

    Ahora la duración del Laser se gestiona fuera del proyectil

--- Assets/Code/Proyectiles/LaserProjectile.cs
+++ Assets/Code/Proyectiles/LaserProjectile.cs
@@ -1,12 +1,9 @@
-using System.Collections;
 using UnityEngine;
 
 public class LaserProjectile : Projectile
 {
     [SerializeField] private float distanceLaser;
     [SerializeField] private LayerMask layerMask;
-    [SerializeField] private float timeToRecharge;
-    [SerializeField] private float laserDuration;
 
     private Transform _laserOrigin;
     private LineRenderer _lineRenderer;
@@ -25,42 +22,28 @@ public class LaserProjectile : Projectile
     {
         if (_isActive == false) return;
 
-        laserDuration -= Time.fixedDeltaTime;
-        if (laserDuration <= 0)
-        {
-            _isActive = false;
-            _lineRenderer.enabled = false;
-            StartCoroutine(RechargeLaser(timeToRecharge));
-        }
-        
         var hit = Physics2D.Raycast(_laserOrigin.position, _laserOrigin.right, distanceLaser,
             layerMask);
         if (hit.collider == null)
         {
-            DrawRay(_laserOrigin.position, MyTransform.right, distanceLaser);    
+            DrawRay(_laserOrigin.position, distanceLaser);    
         }
         else
         {
-            DrawRay(_laserOrigin.position, MyTransform.right, hit.distance);
+            DrawRay(_laserOrigin.position, hit.distance);
         }
     }
 
-    private void DrawRay(Vector2 origin, Vector2 direction, float distance)
+    private void DrawRay(Vector2 origin, float distance)
     {
         _lineRenderer.SetPosition(0, origin);
         var end = new Vector2(origin.x + distance, origin.y);
         _lineRenderer.SetPosition(1, end);
     }
 
-    private IEnumerator RechargeLaser(float seconds)
-    {
-        yield return new WaitForSeconds(seconds);
-        ServiceLocator.Instance.GetService<EventQueue>().EnqueueEvent(new CanShootEvent());
-        DeactivateProjectile();
-    }
-    
     protected override void DoDeactivate()
     {
-        
+        _isActive = false;
+        _lineRenderer.enabled = false;        
     }
 }
