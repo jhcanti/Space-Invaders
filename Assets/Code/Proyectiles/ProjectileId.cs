a4d4c69b1d0b23db19ada8d08d403aea645a796c
jhcanti <jhcanti@gmail.com>
2021-12-31 16:28:56 +0100

    Update ProjectileId.cs

--- Assets/Code/Proyectiles/ProjectileId.cs
+++ Assets/Code/Proyectiles/ProjectileId.cs
@@ -6,10 +6,10 @@ public class ProjectileId : ScriptableObject
     [SerializeField] private string projectileId;
     [SerializeField] private Sprite icon;
     [SerializeField] private float fireRate;
-    [SerializeField] private float costPerSecond;
+    [SerializeField] private float durabilityInSeconds;
 
     public string Value => projectileId;
     public Sprite Icon => icon;
     public float FireRate => fireRate;
-    public float CostPerSecond => costPerSecond;
+    public float DurabilityInSeconds => durabilityInSeconds;
 }
