6307badbe0f80f66c60e03a81952bc909d1dce12
jhcanti <jhcanti@gmail.com>
2022-01-04 21:37:04 +0100

    Ahora la mina explosiva hace daño al explotar

--- Assets/Code/Enemies/ExplosiveMine.cs
+++ Assets/Code/Enemies/ExplosiveMine.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class ExplosiveMine : Enemy
 {
     [SerializeField] private float radiusDetection;
+    [SerializeField] private float radiusExplosion;
     [SerializeField] private LayerMask layerMask;
     [SerializeField] private float durationBlink;
     [SerializeField] private int loops;
+    [SerializeField] private int damageByExplosion;
 
     private SpriteRenderer _myRenderer;
     private bool _isActive;
@@ -33,7 +35,22 @@ public class ExplosiveMine : Enemy
         _sequence.Append(_myRenderer.DOColor(Color.white, durationBlink));
         _sequence.Append(_myRenderer.DOColor(currentColor, durationBlink));
         _sequence.SetLoops(loops);
-        _sequence.OnComplete(() => DestroyEnemy(0));
+        _sequence.OnComplete(() =>
+        {
+            ApplyDamageByExplosion();
+            DestroyEnemy(0);
+        });
+    }
+
+    private void ApplyDamageByExplosion()
+    {
+        var hit = Physics2D.CircleCast(MyTransform.position, radiusExplosion,
+            MyTransform.right, 0f, layerMask);
+        if (hit.collider == null) return;
+        
+        var damageable = hit.collider.GetComponent<IDamageable>();
+
+        damageable?.ReceiveDamage(damageByExplosion);
     }
 
     protected override void DoDestroy()
@@ -51,6 +68,15 @@ public class ExplosiveMine : Enemy
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireSphere(MyTransform.position, 1.5f);
+        if (_isActive == false)
+        {
+            Gizmos.color = Color.white;
+            Gizmos.DrawWireSphere(MyTransform.position, radiusDetection);
+        }
+        else
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(MyTransform.position, radiusExplosion);
+        }
     }
 }
