c03214c42752ad761d46aeaa1fe62f77ac6f4988
jhcanti <jhcanti@gmail.com>
2022-01-06 23:02:37 +0100

    Nuevo enemigo Bug6

---
+++ Assets/Code/Enemies/Bug6.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Bug6 : Enemy
+{
+    [SerializeField] protected AnimationCurve verticalMovement;
+
+    private Vector3 _currentPosition;
+    private float _currentTime;
+    
+    protected override void DoInit()
+    {
+        _currentTime = 0;
+        _currentPosition = MyTransform.position;
+    }
+
+    protected override void DoTryShoot()
+    {
+        
+    }
+
+    protected override void DoMove()
+    {
+        _currentPosition += MyTransform.right * (Speed * Time.deltaTime);
+        var verticalPosition = MyTransform.up * verticalMovement.Evaluate(_currentTime);
+        Rb.MovePosition(_currentPosition + verticalPosition);
+        _currentTime += Time.deltaTime;
+    }
+
+    protected override void DoDestroy()
+    {
+        
+    }
+}
