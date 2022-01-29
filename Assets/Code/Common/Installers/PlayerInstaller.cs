5e47bd17279f8087804667a5e2171dddd1b8ae85
jhcanti <jhcanti@gmail.com>
2022-01-29 10:46:10 +0100

    Update PlayerInstaller.cs

--- Assets/Code/Common/Installers/PlayerInstaller.cs
+++ Assets/Code/Common/Installers/PlayerInstaller.cs
@@ -6,11 +6,17 @@ public class PlayerInstaller : MonoBehaviour
     [SerializeField] private Transform playerSpawnPosition;
 
     private PlayerMediator _player;
-    
+    private EventQueue _eventQueue;
+
+    private void Start()
+    {
+        _eventQueue = ServiceLocator.Instance.GetService<EventQueue>();
+    }
 
     public void SpawnPlayer()
     {
         _player = Instantiate(prefab, playerSpawnPosition.position, Quaternion.identity);
+        _eventQueue.EnqueueEvent(new PlayerSpawnEvent());
     }
 
     public void SetPlayerInactive()
