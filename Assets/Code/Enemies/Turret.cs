0d978971842b530d490758cad3dd0fb7c4b50755
jhcanti <jhcanti@gmail.com>
2022-02-05 10:22:53 +0100

    New movement for Turret

--- Assets/Code/Enemies/Turret.cs
+++ Assets/Code/Enemies/Turret.cs
@@ -4,12 +4,12 @@ public class Turret : Enemy
 {
     [SerializeField] private Animator animator;
     [SerializeField] private Transform cannon;
-    
+    private Vector2 _direction;
     
     
     protected override void DoInit()
     {
-        Rb.velocity = -MyTransform.right * Speed;
+        _direction = -MyTransform.right;
     }
 
     protected override void DoTryShoot()
@@ -22,6 +22,7 @@ public class Turret : Enemy
 
     protected override void DoMove()
     {
+        Rb.MovePosition(Rb.position + _direction * (Speed * Time.fixedDeltaTime));
         var aimDirection = (Player.position - transform.position).normalized;
         var angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
         cannon.rotation = Quaternion.Euler(0f, 0f, angle - 180);
